c2244ddf0921b0a5a4682a0290689e725844c2bd
javabipe <67403018+javabipe@users.noreply.github.com>
2022-08-02 10:42:21 -0300

    Update storyblok.js

--- assets/javascripts/utilities/storyblok.js
+++ assets/javascripts/utilities/storyblok.js
@@ -1,6 +1,6 @@
 // on storyblok we set a component as "page_index" to set some trick for our application index page on Nuxt
 // here the storyblok SLUG name, used as "core" for the trick
 
-const stringNameForHomePage = 'home'
+const stringNameForHomePage = 'homepage'
 
 export default stringNameForHomePage
