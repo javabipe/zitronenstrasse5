5ba8b338fab5dbd74ce984dddb65fb9183c510db
javabipe <67403018+javabipe@users.noreply.github.com>
2022-08-01 22:39:28 -0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
 
 <script>
 import transitionTime from '../assets/javascripts/utilities/transitionTime'
-import stringNameForHomePage from '../assets/javascripts/utilities/storyblok'
+
 
 const timeTransitionCalculation = (transitionTime / 2) * 1.2
 
@@ -29,23 +29,23 @@ export default {
 
     // on stotry block set real path as "/" for homepage. it works (make the trick) on storyblok preview interface
     asyncData(context) {
-        return context.app.$storyapi
-            .get(`cdn/stories/${stringNameForHomePage}`, {
-                // Check if we are in the editor mode
-                version:
-                    context.query._storyblok || context.isDev
-                        ? 'draft'
-                        : 'published',
-            })
-            .then((res) => {
-                return res.data.story
-            })
-            .catch((res) => {
-                context.error({
-                    statusCode: res.response.status,
-                    message: res.response.data,
-                })
-            })
+       const placesList = [
+    {
+        // 001
+        // ChIJX2MutgxPqEcR2nvgHiQ0ojE
+        title: 'Geschwister Nothaft Café',
+        address: 'Schwarzastraße 9, 12055',
+        thumb: '001_nothaftcafes.jpg',
+        thumbCredits: 'Geschwister Nothaft Café FB Page',
+        website: 'http://nothaftcafes.com/',
+        fbPage: 'https://www.facebook.com/geschwisternothaft/',
+        position: {
+            lat: 52.472729,
+            lng: 13.454635,
+        },
+    }
+    ]
+    return placesList
     },
 
     mounted() {
